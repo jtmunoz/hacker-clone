85c85631e3bb925891c13e894d09db156c53ca0e
jtmunoz <jordantmunoz@gmail.com>
2018-08-16 23:03:49 -0700

    adds questions, answer migrations

--- db/migrate/20180814142608_create_questions.rb
+++ db/migrate/20180814142608_create_questions.rb
@@ -1,6 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
+    	t.text :query, null: false
 
       t.timestamps
     end
--- db/migrate/20180814142701_create_answers.rb
+++ db/migrate/20180814142701_create_answers.rb
@@ -1,6 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
+    	t.text :explanation, null: false
 
       t.timestamps
     end
