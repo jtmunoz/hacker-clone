15a0f3750720b04a6a67e54f188b9250f73f784d
jtmunoz <jordantmunoz@gmail.com>
2018-08-18 09:12:48 -0700

    adds to questions and answers migrations

--- db/migrate/20180814142608_create_questions.rb
+++ db/migrate/20180814142608_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
     	t.text :query, null: false
+    	t.integer :author_id, null: false
 
       t.timestamps
     end
--- db/migrate/20180814142701_create_answers.rb
+++ db/migrate/20180814142701_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
     	t.text :explanation, null: false
+    	t.integer :author_id, null: false
 
       t.timestamps
     end
